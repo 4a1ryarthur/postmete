a16b044f348212e30905608741735c69bb751713
4a1ryarthur <legoxbr@gmail.com>
2024-07-31 18:24:44 -0300

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,7 @@
+# This Will Be Installed in Your First 
+# Download.
+luarocks install luasocket
+luarocks install lxsh
+
+# TODO: Install Lua Compiler
+# Delete This File if It's Already Installed 
